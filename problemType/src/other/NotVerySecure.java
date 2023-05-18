1cb82efb32e0b6da99112c7cc4456bd618ce2b60
Aleatmos <a.moskov95@gmail.com>
2023-05-18 11:40:53 +0300

    solutions

---
+++ problemType/src/other/NotVerySecure.java
@@ -0,0 +1,24 @@
+package other;
+
+public class NotVerySecure {
+    public static boolean alphanumeric(String s){
+
+        if (s.isEmpty()) {
+            return false;
+        }
+
+        String[] elements = s.split("");
+
+        for (int i = 0; i < elements.length; i++) {
+            int currElement = elements[i].charAt(0);
+            if (s.trim().length() == 0) {
+                return false;
+            } else if (!(currElement >= 65 && currElement <= 90) && !(currElement >= 97 && currElement <= 122) && !(currElement >= 48 && currElement <= 57)) {
+                return false;
+            } else if (elements[i].equals(" ")) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
